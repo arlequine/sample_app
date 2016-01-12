f6cc3aa72ad833bd03b5ab091de92ebfcd5d757b
arlequine1 <jfredo.n@outlook.com>
2016-01-12 16:50:32 +0000

    fixed layout

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,10 +14,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
-  test "email should not be too long" do
-    @user.email = "a" * 244 + "@example.com"
-    assert_not @user.valid?
-  end
+#  test "email should not be too long" do
+#    @user.email = "a" * 244 + "@example.com"
+#    assert_not @user.valid?
+#  end
   
   test "should be valid" do
     assert @user.valid?
@@ -61,6 +61,13 @@ class UserTest < ActiveSupport::TestCase
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
+  end
+  
+    test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should have a minimum length" do
