7782bfb9653648aed6bf889716cc2dd7c9cc5c87
arlequine1 <jfredo.n@outlook.com>
2016-01-29 03:40:42 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :micropost, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -44,8 +45,7 @@ class User < ActiveRecord::Base
   
     # Activates an account.
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true , activated_at: Time.zone.now)
   end
 
   # Sends activation email.
@@ -55,8 +55,8 @@ class User < ActiveRecord::Base
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token),
+                    reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email.
@@ -66,6 +66,10 @@ class User < ActiveRecord::Base
   
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
     private
--- db/seeds.rb
+++ db/seeds.rb
@@ -34,6 +34,11 @@ User.create!(name:  "Fredo",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
 
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.micropost.create!(content: content) }
 end
 
